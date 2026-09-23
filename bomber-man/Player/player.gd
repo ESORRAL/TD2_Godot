86b9e1b6f811bf220d6414c7eae8c7141df58631
elliotaussel <ausselelliotpoubelle@gmail.com>
2026-09-23 09:12:50 +0200

    création base player

---
+++ bomber-man/Player/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody3D
+
+@export var speed: float = 5.0
+
+func _physics_process(_delta: float) -> void:
+	var input_dir := Input.get_vector("p_left", "p_right", "p_up", "p_down")
+	var direction := Vector3(input_dir.x, 0, input_dir.y).normalized()
+
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+		
+	move_and_slide()
